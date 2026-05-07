1b3d3746b818b57393141af7f51f35326988ce69
hgjeong <hgjeong@mcnex.com>
2026-05-07 10:02:23 +0900

    fix(GitHub Pages): Update base path to match renamed repository

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // GitHub Pages 배포를 위한 base 경로 설정. 리포지토리 이름과 동일해야 합니다.
-  base: '/Dev-Portfolio/',
+  base: '/Hyun9_Portfolio/',
 })
